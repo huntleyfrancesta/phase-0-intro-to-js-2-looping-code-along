8202168321da4bf83cace453d25a7f27a87a9935
francesta <huntleyfrancesta@gmail.com>
2021-03-18 21:51:03 -0400

    git@github.com:huntleyfrancesta/phase-0-intro-to-js-2-looping-code-along.git

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messageArray = []
+    for (let i = 0; i < names.length; i++) {
+        console.log(i)
+        let greeting = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messageArray[i] = greeting;
+    }
+    return messageArray;
+    debugger;
+}
+const num = 10;
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+    return num;
+    l;
+}
+const avent = ["birthday"];
+console.log(writeCards(names, avent))
+    // const names = (["Ada", "Brendan", "Ali"], 'surprise');
+    // const countDown = (10; > = 0)
+    // const avent = "birthday";
+    // const messageArray = []
+    // for (let i = 0; i < names.length; i++) {
+    // let greeting = 'Thank you, ${names[i]}, for the wonderful ${avent} gift!`;
+    // messagesArray[i] = greeting;
+    // }
+    // console.log(messagesArray);
